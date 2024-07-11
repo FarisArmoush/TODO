f05e4d92759e17fc03253f22d3346faafea7e9c8
FarisArmoush <fariskarmoush@gmail.com>
2024-07-11 13:52:33 +0300

    Initial Commit

---
+++ TODO/TODOApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOApp.swift
+//  TODO
+//
+//  Created by Faris Armoush on 11/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct TODOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
